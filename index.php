c665f7436dd7289628eea556f1b19ff1cd15996f
Siripiri-hue <daemonprocellae@live.it>
2022-04-15 11:50:56 +0200

    Aggiunto metodo

--- index.php
+++ index.php
@@ -19,14 +19,24 @@ class Movie {
         $this->distribution_home = $_distribution_home;
     }
 
+    public function setDetails ($original_title, $cast, $genre, $music_by) {
+        $this->original_title = $original_title;
+        $this->cast = $cast;
+        $this->genre = $genre;
+        $this->music_by = $music_by;
+    }
+
     public function getDetails() {
         return [$this->title, $this->year, $this->country, $this->direct_by, $this->distribution_home];
     }
 }
 
-$pulp_fiction = new Movie ('Pulp Fiction', '1994', 'USA', 'Q. Tarantino', 'Miramax');
+$pulp_fiction = new Movie ('Pulp Fiction', '1994', 'USA', 'Quentin Tarantino', 'Miramax');
+$pulp_fiction->setDetails('Pulp Fiction', ['John Travolta', 'Uma Turman', 'Samuel L. Jackson', 'Bruce Willis'], ['noir', 'thriller', 'drama'], 'aa.vv.');
 var_dump($pulp_fiction);
+
 $prova_prendermi = new Movie ('Prova a prendermi', '2002', 'USA', 'S. Spielberg', 'United International');
+$prova_prendermi->setDetails('Catch me if you can', ['Leonardo di Caprio', 'Tom Hanks', 'Christopher Walken'], ['commedia', 'drama'], 'John Williams');
 var_dump($prova_prendermi);
 
 // echo $pulp_fiction->getDetails();
