e3e07a15449335f2c8c761adfd442d81c069821d
Siripiri-hue <daemonprocellae@live.it>
2022-04-15 11:18:04 +0200

    Aggiunto costruttore

--- index.php
+++ index.php
@@ -8,4 +8,11 @@ class Movie {
     public $genre = [];
     public $music_by;
     public $distribution;
+
+    public __constructor ($_title, $_year, $_distribution) {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->distribution = $_distribution;
+    }
 }
+
