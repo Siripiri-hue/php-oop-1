647d6bc4dd9ed267775e6fae2f85f5a2c3eb7227
Siripiri-hue <daemonprocellae@live.it>
2022-04-15 11:08:00 +0200

    Aggiunta classe

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+class Movie {
+    public $title;
+    public $year;
+    public $direct_by;
+    public $cast = [];
+    public $genre = [];
+    public $music_by;
+    public $distribution;
+}
