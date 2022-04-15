9d6e465efa42c89c2b2de6a94067cf19f97c438b
Siripiri-hue <daemonprocellae@live.it>
2022-04-15 11:30:34 +0200

    Sistemati dettagli

--- index.php
+++ index.php
@@ -4,18 +4,21 @@ class Movie {
     public $title;
     public $original_title;
     public $year;
+    public $country;
     public $direct_by;
     public $cast = [];
     public $genre = [];
     public $music_by;
-    public $distribution;
+    public $distribution_home;
 
-    public __constructor ($_title, $_year, $_distribution) {
+    public __constructor ($_title, $_year, $_country; $_directed_by, $_distribution_home) {
         $this->title = $_title;
         $this->year = $_year;
-        $this->distribution = $_distribution;
+        $this->country = $_country;
+        $this->$direct_by = $_directed_by;
+        $this->distribution_home = $_distribution_home;
     }
 }
 
-$pulp_fiction = new Movie ('Pulp Fiction', '1994', 'Q. Tarantino');
-&prova_prendermi = new Movie ('Prova a prendermi', '2002', 'S. Spielberg');
+$pulp_fiction = new Movie ('Pulp Fiction', '1994', 'USA', 'Q. Tarantino', 'Miramax');
+$prova_prendermi = new Movie ('Prova a prendermi', '2002', 'USA', 'S. Spielberg', 'United International');
